1ad6acb06c3bd966a8eec4c4f18e09167c251459
Alpha <adiallo@exomind.fr>
2024-05-03 10:19:20 +0200

    feature : call working

--- app/src/main/java/com/alphaomardiallo/pawnedemail/feature/getallbreaches/data/remote/api/GetAllBreachesHIBPApi.kt
+++ app/src/main/java/com/alphaomardiallo/pawnedemail/feature/getallbreaches/data/remote/api/GetAllBreachesHIBPApi.kt
@@ -3,13 +3,13 @@ package com.alphaomardiallo.pawnedemail.feature.getallbreaches.data.remote.api
 import com.alphaomardiallo.pawnedemail.common.data.util.ApiResponse
 import com.alphaomardiallo.pawnedemail.feature.getallbreaches.data.model.AllBreachesDto
 import retrofit2.http.GET
-import retrofit2.http.Query
+import retrofit2.http.Path
 
 interface GetAllBreachesHIBPApi {
 
-    @GET(BASE_URL)
+    @GET("$BASE_URL{account}")
     suspend fun getAllBreachesHIBP(
-        @Query("account") emailAddress: String
+        @Path("account") emailAddress: String
     ): ApiResponse<AllBreachesDto>
 
     private companion object {
