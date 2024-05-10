21ed5f43701c2635f2a8adc6fff70688086b4e81
Alpha <adiallo@exomind.fr>
2024-05-10 10:18:57 +0200

    feature : ready for v1

--- app/src/main/java/com/alphaomardiallo/pawnedemail/feature/secure/domain/SecureSteps.kt
+++ app/src/main/java/com/alphaomardiallo/pawnedemail/feature/secure/domain/SecureSteps.kt
@@ -2,25 +2,29 @@ package com.alphaomardiallo.pawnedemail.feature.secure.domain
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import com.alphaomardiallo.pawnedemail.R
 
 sealed class SecureSteps(
     @DrawableRes val image: Int,
     @StringRes val contentDescription: Int,
+    @StringRes val title: Int,
     @StringRes val text: Int,
-    @StringRes val link: Int,
+    @StringRes val link: Int?,
 ) {
 
-    data object PassWord : SecureSteps(
-        image = 0,
-        contentDescription = 0,
-        text = 0,
-        link = 0
+    data object Password : SecureSteps(
+        image = R.drawable.password_data_img,
+        contentDescription = R.string.step_password_content_description,
+        title = R.string.step_password_title,
+        text = R.string.step_password_text,
+        link = null
     )
 
     data object TwoFactorAuth : SecureSteps(
-        image = 0,
-        contentDescription = 0,
-        text = 0,
-        link = 0
+        image = R.drawable.two_factor_auth_data_img,
+        contentDescription = R.string.step_two_factor_auth_content_description,
+        title = R.string.step_two_factor_auth_title,
+        text = R.string.step_two_factor_auth_text,
+        link = null
     )
 }
