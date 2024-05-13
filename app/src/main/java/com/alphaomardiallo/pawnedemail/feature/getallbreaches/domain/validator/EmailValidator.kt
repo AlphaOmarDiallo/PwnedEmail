f8214b400bff43baef62c34cca4a2bbcddaf54b4
Alpha <adiallo@exomind.fr>
2024-05-13 16:03:20 +0200

    feature : validator regex added and test added

--- app/src/main/java/com/alphaomardiallo/pawnedemail/feature/getallbreaches/domain/validator/EmailValidator.kt
+++ app/src/main/java/com/alphaomardiallo/pawnedemail/feature/getallbreaches/domain/validator/EmailValidator.kt
@@ -5,15 +5,15 @@ import java.util.regex.Pattern
 object EmailValidator {
 
     /**
-     * Regular expression pattern for validating email addresses.
-     * - ^ asserts the start of the line.
-     * - [a-zA-Z0-9._%+-]+ matches one or more of any alphanumeric characters, dot (.), underscore (_), percent (%),
-     *   plus (+), or hyphen (-).
-     * - @ matches the "@" symbol.
-     * - [a-zA-Z0-9.-]+ matches one or more of any alphanumeric characters, dot (.), or hyphen (-) after the "@" symbol.
-     * - \\. matches a dot (.) after the domain name.
-     * - [a-zA-Z]{2,} matches two or more of any alphabetic characters for the top-level domain (TLD).
-     * - $ asserts the end of the line.
+     * Regex pattern for validating email addresses
+     * Explanation:
+     * ^                   // Start of the string
+     * [a-zA-Z0-9._%+-]+   // Match one or more of the following characters: letters (both cases), numbers, dot, underscore, percent, plus, or hyphen
+     * @                   // Match the "@" symbol
+     * [a-zA-Z0-9.-]+      // Match one or more of the following characters: letters (both cases), numbers, dot, or hyphen
+     * \.                  // Match a literal dot (.)
+     * [a-zA-Z]{2,}        // Match two or more letters (both cases)
+     * $                   // End of the string
      */
     private const val EMAIL_REGEX_PATTERN = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"
 
