f5a8901e342a1f671cc4ba695439f2c74206908a
Alpha <adiallo@exomind.fr>
2024-05-13 23:22:12 +0200

    feature : database entities tested

---
+++ app/src/test/java/com/alphaomardiallo/pawnedemail/common/data/local/entity/EmailEntityTest.kt
@@ -0,0 +1,26 @@
+package com.alphaomardiallo.pawnedemail.common.data.local.entity
+
+import com.alphaomardiallo.pawnedemail.common.domain.model.Email
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class EmailEntityTest {
+
+    val emailEntity = EmailEntity(
+        id = 0,
+        email = "x@yopmail.com"
+    )
+
+    @Test
+    fun `ItemEmailEntity can be transformed to Email`() {
+        val toEmail = emailEntity.toEmail()
+        assertThat(toEmail).isInstanceOf(Email::class.java)
+    }
+
+    @Test
+    fun `ItemEmailEntity when transformed to Email has the same data`() {
+        val toEmail = emailEntity.toEmail()
+
+        assertThat(emailEntity.email).isEqualTo(toEmail.email)
+    }
+}
